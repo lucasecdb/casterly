de03390ad3b8a6040487c093b32b83e90586a737
Lucas Cordeiro <ecdb.lucas@gmail.com>
2019-09-27 18:26:43 -0300

    connect to chrome devtools inspector

--- src/start-dev.ts
+++ src/start-dev.ts
@@ -1,4 +1,7 @@
+/* eslint-disable no-console */
 import express from 'express'
+import inspector from 'inspector'
+import chalk from 'chalk'
 
 import server from './index'
 import proxyMiddleware from './middlewares/proxy'
@@ -9,6 +12,25 @@ import { logStore } from './output/logger'
 const PORT = 3000
 
 const start = () => {
+  try {
+    inspector.open()
+    console.log(
+      chalk`
+{dim
+  {green Debugger started on ${inspector.url()}}
+}`.trim()
+    )
+  } catch (err) {
+    const errorStr = chalk`
+{dim
+  {red Could not establish a connection with the inspector}
+  {gray ${err}}
+}
+    `.trim()
+
+    console.error(errorStr)
+  }
+
   const app = express()
 
   proxyMiddleware.set(app)
