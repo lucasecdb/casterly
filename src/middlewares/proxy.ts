365fb8ee81a8787dd0533c69aee2a2189b20cbea
Lucas Cordeiro <ecdb.lucas@gmail.com>
2019-09-27 18:40:40 -0300

    update proxy middleware to use config from package.json

--- src/middlewares/proxy.ts
+++ src/middlewares/proxy.ts
@@ -1,18 +1,21 @@
 import express, { Application } from 'express'
 import proxy from 'http-proxy-middleware'
 
+import * as paths from '../../config/paths'
+
 export default {
   set: (app: Application) => {
-    app.use(
-      '/_c',
-      proxy({
-        target: 'http://localhost:5000',
-        pathRewrite: {
-          '^/_c': '',
-        },
-        logLevel: 'silent',
-      })
-    )
+    const appPackageJson = require(paths.appPackageJson)
+
+    if (appPackageJson.proxy) {
+      app.use(
+        appPackageJson.proxy.path,
+        proxy({
+          ...appPackageJson.proxy.config,
+          logLevel: 'silent',
+        })
+      )
+    }
 
     app.use('/', express.static('public'))
   },
