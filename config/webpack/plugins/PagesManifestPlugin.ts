56841719509c793920d8569ccd810c8c84238fae
Lucas Cordeiro <ecdb.lucas@gmail.com>
2019-11-05 01:17:32 -0300

    fix error in pages manifest plugin

--- config/webpack/plugins/PagesManifestPlugin.ts
+++ config/webpack/plugins/PagesManifestPlugin.ts
@@ -5,29 +5,32 @@ import { PAGES_MANIFEST_FILE } from '../../constants'
 // This plugin creates a pages-manifest.json from page entrypoints.
 export default class PagesManifestPlugin implements Plugin {
   apply(compiler: Compiler): void {
-    compiler.hooks.emit.tap('PagesManifestPlugin', (compilation, callback) => {
-      const { chunks } = compilation
-      const pages: { [page: string]: string } = {}
+    compiler.hooks.emit.tapAsync(
+      'PagesManifestPlugin',
+      (compilation, callback) => {
+        const { chunks } = compilation
+        const pages: { [page: string]: string } = {}
 
-      for (const chunk of chunks) {
-        const pagePath = chunk.name
+        for (const chunk of chunks) {
+          const pagePath = chunk.name
 
-        if (!pagePath) {
-          continue
+          if (!pagePath) {
+            continue
+          }
+
+          // Write filename, replace any backslashes in path (on windows) with forwardslashes for cross-platform consistency.
+          pages[`/${pagePath.replace(/\\/g, '/')}`] = chunk.name.replace(
+            /\\/g,
+            '/'
+          )
         }
 
-        // Write filename, replace any backslashes in path (on windows) with forwardslashes for cross-platform consistency.
-        pages[`/${pagePath.replace(/\\/g, '/')}`] = chunk.name.replace(
-          /\\/g,
-          '/'
+        compilation.assets[PAGES_MANIFEST_FILE] = new RawSource(
+          JSON.stringify(pages, null, 2)
         )
+
+        callback()
       }
-
-      compilation.assets[PAGES_MANIFEST_FILE] = new RawSource(
-        JSON.stringify(pages, null, 2)
-      )
-
-      callback()
-    })
+    )
   }
 }
