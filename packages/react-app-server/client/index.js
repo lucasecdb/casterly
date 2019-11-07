94d9713a317c5d7d6591092121c58e86271c1724
Lucas Cordeiro <ecdb.lucas@gmail.com>
2019-11-07 09:34:02 -0300

    fix nossr query in client

--- packages/react-app-server/client/index.js
+++ packages/react-app-server/client/index.js
@@ -1,5 +1,13 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+
 import Component from '#app/index'
 
-ReactDOM.hydrate(<Component />, document.getElementById('root'))
+const query = new URLSearchParams(window.location.search)
+
+const shouldHydrate =
+  !query.has('nossr') || process.env.NODE_ENV === 'production'
+
+const method = shouldHydrate ? 'hydrate' : 'render'
+
+ReactDOM[method](<Component />, document.getElementById('root'))
