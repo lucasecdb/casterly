c257ca1537991551173a3509744260869a5005b3
Lucas Cordeiro <ecdb.lucas@gmail.com>
2020-11-17 22:35:59 -0300

    Fix main entrypoint assets

--- packages/components/src/Styles.tsx
+++ packages/components/src/Styles.tsx
@@ -3,11 +3,12 @@ import React from 'react'
 import { useRootContext } from './RootContext'
 
 export const Styles: React.FC = () => {
-  const { matchedRoutesAssets } = useRootContext()
+  const { matchedRoutesAssets, mainAssets } = useRootContext()
 
   return (
     <>
       {matchedRoutesAssets
+        .concat(mainAssets)
         .filter((file) => file.endsWith('.css'))
         .map((file) => (
           <link key={file} rel="stylesheet" type="text/css" href={file} />
