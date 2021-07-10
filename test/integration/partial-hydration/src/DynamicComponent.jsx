5952791e2271fd03eaa79bfb6f271dd1540443ec
Lucas Cordeiro <ecdb.lucas@gmail.com>
2021-07-10 16:01:23 -0300

    Attempt to fix flaky test (#442)
    * Attempt to fix flaky test
    
    * Update test assertions

--- test/integration/partial-hydration/src/DynamicComponent.jsx
+++ test/integration/partial-hydration/src/DynamicComponent.jsx
@@ -1,5 +1,19 @@
+import { useState } from 'react'
+
 const DynamicComponent = () => {
-  return <div>Hello from dynamic component!</div>
+  const [changed, setChanged] = useState(false)
+
+  return (
+    <div>
+      <p>
+        {!changed
+          ? 'Hello from dynamic component!'
+          : 'Hello from alternate message!'}
+      </p>
+
+      <button onClick={() => setChanged(true)}>change message</button>
+    </div>
+  )
 }
 
 export default DynamicComponent
