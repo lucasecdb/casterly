bc142d809501a3d17cb6da4909c5e78c1e4ec0c5
Lucas Cordeiro <ecdb.lucas@gmail.com>
2021-10-12 14:41:26 -0300

    Only use React 18 in react-18 tests

---
+++ test/integration/react-18/webpack.config.js
@@ -0,0 +1,14 @@
+module.exports = (config) => {
+  const alias = {}
+
+  // FIXME: resolving react/jsx-runtime https://github.com/facebook/react/issues/20235
+  alias['react/jsx-dev-runtime'] = require.resolve('react/jsx-dev-runtime.js')
+  alias['react/jsx-runtime'] = require.resolve('react/jsx-runtime.js')
+
+  alias['react'] = 'react-18'
+  alias['react-dom'] = 'react-dom-18'
+
+  config.resolve.alias = alias
+
+  return config
+}
