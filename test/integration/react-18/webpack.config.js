e13158409991fceeb840a96f35df816494e516b4
Lucas Cordeiro <ecdb.lucas@gmail.com>
2021-10-19 22:00:18 -0300

    Fix react-18 test config

--- test/integration/react-18/webpack.config.js
+++ test/integration/react-18/webpack.config.js
@@ -1,5 +1,5 @@
 module.exports = (config) => {
-  const alias = {}
+  const alias = { ...config.resolve.alias }
 
   // FIXME: resolving react/jsx-runtime https://github.com/facebook/react/issues/20235
   alias['react/jsx-dev-runtime'] = require.resolve('react/jsx-dev-runtime.js')
